9b0a6377a828c3682c8edb52c34b892ffc29cc7a
Stefano Raspa <stefanoraspa@users.noreply.github.com>
2026-09-13 18:50:33 +0200

    Add PWA capabilities: manifest, icons, service worker and install guide

---
+++ static/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'rebel-feed-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/static/manifest.json',
+  '/static/img/iniziativalibertaria.jpg',
+  '/static/img/amicizapatisti.jpg',
+  '/static/img/icon-192.png',
+  '/static/img/icon-512.png',
+  '/static/img/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+  }
+});
